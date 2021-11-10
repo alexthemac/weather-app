851dd579790d2e8ad21a6a405575a13c301afd21
Alex MacLure <almaclure@gmail.com>
2021-11-09 19:12:43 -0500

    Install axios and dotenv. Add openweather api key.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+require('dotenv').config();
+
 
 function App() {
+
+  useEffect(() => {
+
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`)
+    .then(response => {
+      console.log(response);
+    });
+
+  }, []);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
